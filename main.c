d8b602751d79d044c851b8d814d079be5fe72758
Bastien Guillaume <bguillau@student.42.fr>
2023-06-13 14:17:58 +0200

    main simple avec readline

---
+++ main.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bguillau <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/17 15:10:02 by bguillau          #+#    #+#             */
+/*   Updated: 2023/06/13 14:09:15 by bguillau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	main(int argc, char **argv, char **envp)
+{
+	char *line;
+
+	if (!argc || !envp)
+		return (1);
+	while (TRUE)
+	{
+		line = readline("minishell prompt % ");
+		printf("--> %s\n", line);
+		free(line);
+	}
+}
---
+++ minishell.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+/* compilation with -lreadline */
+
+#define TRUE 1
