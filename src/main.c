aabadca2a058aa549e72456e3d5d470ee7446ff0
Bastien Guillaume <bguillau@student.42.fr>
2023-06-13 14:24:40 +0200

    libft, src, inc and miakefile

---
+++ src/main.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bguillau <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/17 15:10:02 by bguillau          #+#    #+#             */
+/*   Updated: 2023/06/13 14:22:21 by bguillau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/minishell.h"
+
+int	main(int argc, char **argv, char **envp)
+{
+	char *line;
+
+	(void) argv;
+	if (!argc || !envp)
+		return (1);
+	while (TRUE)
+	{
+		line = readline("minishell prompt % ");
+		printf("--> %s\n", line);
+		free(line);
+	}
+}
