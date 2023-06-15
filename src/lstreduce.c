52dc73a51b5cf706f4abcbb0ed05b5105c9cac8b
Antonin GUYON <aguyon@bess-f2r3s6.clusters.42paris.fr>
2023-06-15 19:57:21 +0200

    prend ca

--- src/lstreduce.c
+++ src/lstreduce.c
@@ -6,7 +6,7 @@
 /*   By: aguyon <aguyon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 19:02:59 by aguyon            #+#    #+#             */
-/*   Updated: 2023/06/15 15:22:35 by aguyon           ###   ########.fr       */
+/*   Updated: 2023/06/15 19:55:33 by aguyon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,57 +17,26 @@ int	isstrquote(const char *str)
 	return (strcmp(str, "\"") == 0 || strcmp(str, "\'") == 0);
 }
 
-int	isstrquote_g(void *str)
+int	isstroperator(const char *str)
 {
-	return (isstrquote((const char *)str));
+	return (strcmp(str, "&") == 0 || strcmp(str, "|") == 0);
 }
 
-void *ft_strdup_g(void *str)
+int isstrspace(const char *str)
 {
-	return ((void *)ft_strdup((const char *)str));
+	return (strcmp(str, " ") == 0 || strcmp(str, "\t") == 0);
 }
 
-void *ft_strjoin_g(void *s1, void *s2)
+t_llist	*llstreduce_range(t_llist *begin, t_llist *end)
 {
-	return ((void *)ft_strjoin((const char *)s1, (const char *)s2));
-}
-
-int	reduce_word(t_llist	*begin)
-{
-	t_llist	*end;
 	t_llist	*temp;
-	t_llist	*new;
 	char	*str;
 
-	end = llstfind_if(begin->next, isstrquote_g);
-	if (end == NULL)
-		return (0);
-	end = end->next;
 	temp = llstmap_range(begin, end, (void *(*)(void *))ft_strdup, free);
 	if (temp == NULL)
-		return (-1);
+		return (NULL);
 	str = llstfold(temp, ft_strdup(""), (void *(*)(void *, void *))ft_strjoin, free);
-	new = llstnew(str);
-	if (new == NULL)
-		return (-1);
-	llstadd_before(&begin, new);
-	llstclear(&temp, free);
-	llstremove_range(&(begin->next), end, free);
-	return (1);
-}
-
-void	lstreduce(t_llist	**llst)
-{
-	t_llist	*current;
-
-	current = *llst;
-
-	while (current != NULL)
-	{
-		if (isstrquote_g(current->content))
-		{
-			reduce_word(current);
-		}
-		current = current->next;
-	}
+	if (str == NULL)
+		return (llstclear(&temp, free), NULL);
+	return (llstclear(&temp, free), llstnew(str));
 }
