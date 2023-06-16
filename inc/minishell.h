642b38d2e9346ff94e4a78ded1511bc30089faa2
Antonin GUYON <aguyon@bess-f2r4s5.clusters.42paris.fr>
2023-06-16 14:23:05 +0200

    modification lsttok pour nouvelle version

--- inc/minishell.h
+++ inc/minishell.h
@@ -1,20 +1,36 @@
-#ifndef MINISHELL_H
-#define MINISHELL_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aguyon <aguyon@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/16 14:20:52 by aguyon            #+#    #+#             */
+/*   Updated: 2023/06/16 14:21:26 by aguyon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include "../llist/llist.h"
-#include "../libft/libft.h"
-#include "../llist/llist.h"
-#include <readline/readline.h>
-#include <readline/history.h>
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include "../libft/libft.h"
+# include "../llist/llist.h"
+# include <readline/readline.h>
+# include <readline/history.h>
 /* compilation with -lreadline */
 
-#define TRUE 1
-#define DELIM " |&\'\"()=$"
+# define TRUE 1
+# define DELIM " \t|&()"
 
-
-t_llist	*lsttok(const char *str, const char *delim);
+t_llist	*lsttok(const char *str);
 void	lstreduce(t_llist	**llst);
+
+//lsttok_utils
+char	*strfind_if(const char *str, int (*f)(int));
+char	*strfind(const char *str, int c);
+char	*strfind_not(const char *str, int c);
+int		isdelim(int c);
 
 #endif
--- src/lsttok.c
+++ src/lsttok.c
@@ -5,47 +5,41 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: aguyon <aguyon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/13 13:58:07 by aguyon            #+#    #+#             */
-/*   Updated: 2023/06/14 16:07:34 by aguyon           ###   ########.fr       */
+/*   Created: 2023/06/16 14:08:11 by aguyon            #+#    #+#             */
+/*   Updated: 2023/06/16 14:22:31 by aguyon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../libft/libft.h"
-#include "../llist/llist.h"
+#include "../inc/minishell.h"
 
-static size_t	_substr_len(const char *str, const char *delim)
+static size_t	_substr_len(const char *str)
 {
 	size_t	len;
 
-	len = 0;
-	while (!ft_strchr(delim, *str))
-	{
-		len++;
-		str++;
-	}
+	if (ft_strchr("()", *str) != NULL)
+		len = 1;
+	else if (ft_strchr(DELIM, *str))
+		len = _strfind_not(str, *str) - str;
+	else if (*str == '\'')
+		len = _strfind(str + 1, '\'') - str + 1;
+	else if (*str == '\"')
+		len = _strfind(str + 1, '\"') - str + 1;
+	else
+		len = _strfind_if(str, isdelim) - str;
 	return (len);
 }
 
-static char	*_get_next_token(const char **str, const char *delim)
+static char	*_get_next_token(const char **str)
 {
-	char	*token;
-	size_t	len;
+	const size_t	len = _substr_len(*str);
+	char			*token;
 
-	if (ft_strchr(delim, **str))
-	{
-		token = ft_strdup((char []){**str, '\0'});
-		*str += 1;
-	}
-	else
-	{
-		len = _substr_len(*str, delim);
-		token = ft_substr(*str, 0, len);
-		*str += len;
-	}
+	token = ft_substr(*str, 0, len);
+	*str += len;
 	return (token);
 }
 
-t_llist	*lsttok(const char *str, const char *delim)
+t_llist	*lsttok(const char *str)
 {
 	t_llist	*llist;
 	t_llist	*new;
@@ -56,7 +50,7 @@ t_llist	*lsttok(const char *str, const char *delim)
 		return (NULL);
 	while (*str)
 	{
-		token = _get_next_token(&str, delim);
+		token = _get_next_token(&str);
 		if (token == NULL)
 			return (llstclear(&llist, free), NULL);
 		new = llstnew(token);
