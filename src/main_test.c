3d6acb37d8541867d1d9843dec3235c501dccfd4
Bastien Guillaume <bguillau@student.42.fr>
2023-06-28 16:55:43 +0200

    test removequotes prep

--- src/main_test.c
+++ src/main_test.c
@@ -1,6 +1,23 @@
 #include "../inc/minishell.h"
 
+
+//quote rm
 int	main(int argc, char **argv, char **envp)
+{
+	char	*str = ft_strdup("\'salut\'");
+
+	if (argc > 1)
+	{
+		free(str);
+		str = ft_strdup(argv[1]);
+	}
+	str = rm_peer_quotes(str);
+	ft_fprintf(1, "%s\n", str);
+	free(str);
+}
+
+//redire file expansion
+/*int	main(int argc, char **argv, char **envp)
 {
 	char	*str = ft_strdup("\'$USERsalut\'");
 
@@ -12,7 +29,7 @@ int	main(int argc, char **argv, char **envp)
 	str = expand_dollar_redir_file(str, envp);
 	ft_fprintf(1, "%s\n", str);
 	free(str);
-}
+}*/
 
 
 // normal dollar expansion
