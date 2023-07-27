8f436160492ec84e4783f37a6b4de76295ffa26d
Antonin GUYON <antonin.guyon861@gmail.com>
2023-07-27 13:39:21 +0200

    protection manage_pipeline

--- src/manage_pipeline.c
+++ src/manage_pipeline.c
@@ -6,29 +6,35 @@
 /*   By: aguyon <aguyon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 15:14:06 by aguyon            #+#    #+#             */
-/*   Updated: 2023/07/25 15:18:17 by aguyon           ###   ########.fr       */
+/*   Updated: 2023/07/27 13:38:56 by aguyon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-void	manage_pipeline(t_ntree *ast, char **envp)
+int	manage_pipeline(t_ntree *ast, char **envp)
 {
 	t_token	*token;
 	t_llist	*current;
 
 	if (ast == NULL)
-		return ;
+		return (1);
 	token = get_token(ast);
 	if (token->type == PIPELINE)
+	{
 		token->data = get_pipex_info(ast, envp);
+		if (token->data == NULL)
+			return (0);
+	}
 	else
 	{
 		current = ast->children;
 		while (current != NULL)
 		{
-			manage_pipeline(current->content, envp);
+			if (!manage_pipeline(current->content, envp))
+				return (0);
 			current = current->next;
 		}
 	}
+	return (1);
 }
